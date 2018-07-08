ed8be9985f65ce523f0496e490f1834b45f6b184
KristianMariyanov <kristian.mariyanov@gmail.com>
2018-07-09 01:24:34 +0300

    Added button for testing create poll method from eth smart contract;

--- src/web/TrustyPoll/TrustyPoll.Web/src/app/components/home/home.component.ts
+++ src/web/TrustyPoll/TrustyPoll.Web/src/app/components/home/home.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import { TrustyPollService } from '../../services/index'
+
 @Component({
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-    constructor() { }
+    constructor(private trustyPollService: TrustyPollService) { }
+
+    createPoll() {
+        const title = 'title';
+        const gas = 30;
+        this.trustyPollService.createPoll(title, gas).subscribe(tx => console.log(tx));
+    }
 }
