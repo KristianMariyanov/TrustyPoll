8d0a12c5bf12ba1454997effb598b4f3da436f7b
KristianMariyanov <kristian.mariyanov@gmail.com>
2018-07-08 13:01:33 +0300

    Added empty web project and angular template with setup automations

---
+++ src/web/TrustyPoll/TrustyPoll.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrustyPoll.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
