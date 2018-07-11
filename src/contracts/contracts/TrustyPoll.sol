744eebeadd8656d6702b018eef4497215673c7a2
KristianMariyanov <kristian.mariyanov@gmail.com>
2018-07-12 01:07:03 +0300

    added token for creating polls; extended the contract

--- src/contracts/contracts/TrustyPoll.sol
+++ src/contracts/contracts/TrustyPoll.sol
@@ -19,35 +19,75 @@ contract SafeMath {
   }
 }
 
+contract Token {
+  /// @return total amount of tokens
+  function totalSupply() public view returns (uint256 supply) {}
+
+  /// @param _owner The address from which the balance will be retrieved
+  /// @return The balance
+  function balanceOf(address _owner) public view returns (uint256 balance) {}
+
+  /// @notice send `_value` token to `_to` from `msg.sender`
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transfer(address _to, uint256 _value) public returns (bool success) {}
+
+  /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+  /// @param _from The address of the sender
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {}
+
+  /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+  /// @param _spender The address of the account able to transfer  tokens
+  /// @param _value The amount of wei to be approved for transferthe
+  /// @return Whether the approval was successful or not
+  function approve(address _spender, uint256 _value) public returns (bool success) {}
+
+  /// @param _owner The address of the account owning tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @return Amount of remaining tokens allowed to spent
+  function allowance(address _owner, address _spender) public view returns (uint256 remaining) {}
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  uint public decimals;
+  string public name;
+}
+
 contract TrustyPoll is SafeMath {
   address public admin; //the admin address
   address public feeAccount; //the account that will receive fees
-  uint public fee;
+  address public tokenAddress; // the address of the TRP Token
+  uint public fee; // fee for creating polls in TRP Tokens
   uint private optionId;
   uint private pollId;
 
-  constructor(address admin_, address feeAccount_, uint fee_) public {
+  constructor(address admin_, address feeAccount_, address tokenAddress_, uint fee_) public {
     admin = admin_;
     feeAccount = feeAccount_;
+    tokenAddress = tokenAddress_;
     fee = fee_;
   }
   
   struct Poll {
       uint id;
-      string title;
+      bytes32 title;
     }
 
  struct Option {
       uint id;
-      string title;
-      uint pollId;
+      bytes32 title;
     }
     
-    mapping(uint => mapping(address => uint)) public votes; //(poll -> (user -> option)
-    mapping(uint => mapping(uint => uint)) public pollVotesCount; //(poll -> (option -> votesCount)
-    mapping(uint => Option[]) public pollOptions; //(poll -> (user -> option)
-    mapping(uint => address) public pollAuthors; //(poll -> (user -> option)
-    mapping(uint => string) public polls; // pollId -> pollTitle
+    mapping(uint => mapping(address => uint)) public votes; //poll -> (user -> option)
+    mapping(uint => mapping(uint => uint)) public pollVotesCount; //poll -> (option -> votesCount)
+    mapping(uint => Option[]) public pollOptions; //poll -> options[]
+    mapping(uint => address) public pollAuthors; //poll -> author
+    mapping(uint => bytes32) public polls; // pollId -> pollTitle
   
   modifier onlyAdmin() {
     require(msg.sender == admin);
@@ -67,17 +107,22 @@ contract TrustyPoll is SafeMath {
     fee = fee_;
   }
   
-  function createPoll(string title) public {
+  function createPoll(bytes32 title) public {
       require(title != '');
+
+    //ensure approve() method of TRP Token is called before creating poll or this contract will not be able to do the transfer on your behalf.
+    require(Token(tokenAddress).transferFrom(msg.sender, this, fee));
+
       pollId = safeAdd(pollId, 1);
       polls[pollId] = title;
       pollAuthors[pollId] = msg.sender;
   }
   
-  function createOption(uint poll, string title) public {
+  function createOption(uint poll, bytes32 title) public {
+      require(title != '');
       require(pollAuthors[poll] == msg.sender);
       optionId  = safeAdd(optionId, 1);
-      pollOptions[poll].push(Option(optionId, title, poll));
+      pollOptions[poll].push(Option(optionId, title));
   }
   
   function vote(uint poll, uint option) public {
@@ -94,5 +139,13 @@ contract TrustyPoll is SafeMath {
     
     // override previous response
     votes[poll][msg.sender] = option;
+  }
+
+  function getOptionTitleForPoll(uint poll, uint optionIndex) public view returns(bytes32){
+       return pollOptions[poll][optionIndex].title;
+  }
+
+  function getOptionIdForPoll(uint poll, uint optionIndex) public view returns(uint){
+       return pollOptions[poll][optionIndex].id;
   }
 }
--- src/contracts/migrations/1_initial_migration.js
+++ src/contracts/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-var DexETH = artifacts.require("./TrustyPoll.sol");
+var TrustyPoll = artifacts.require("./TrustyPoll.sol");
+var TrustyPollToken = artifacts.require("./TrustyPollToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(DexETH, "0x9ed8edC3633660cD188469175334bDA6C782252B", "0x9ed8edC3633660cD188469175334bDA6C782252B", 0);
+module.exports = function(deployer) {	
+  // deployer.deploy(TrustyPollToken);
+  deployer.deploy(TrustyPoll, "0x9ed8edC3633660cD188469175334bDA6C782252B", "0x9ed8edC3633660cD188469175334bDA6C782252B", "0xb5e15631d41b5b32b0d792cba83e83e63d530e4c",  100000000000000000000);
 };
